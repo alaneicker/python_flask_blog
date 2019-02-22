059391284fc4c5e11be633c2174187c9f3eac6cd
Alan Eicker <alaneicker@gmail.com>
2019-02-22 17:34:46 -0600

    Code formatting

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -18,45 +18,46 @@ posts = [
   }
 ]
 
+# Home route
+# -----------------------
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template(
-      'home.html', 
-      posts=posts, 
-      title='Home'
-    )
+    return render_template('home.html', posts=posts, title='Home')
+
+# About route
+# -----------------------
 
 @app.route("/about")
 def about():
-    return render_template(
-      'about.html', 
-      title='About'
-    )
+  return render_template('about.html', title='About')
+
+# Register route
+# -----------------------
 
 @app.route("/register", methods=['GET','POST'])
 def register():
   form = RegistrationForm()
+  
   if form.validate_on_submit():
     flash(f'Account created for {form.username.data}!', 'success')
     return redirect(url_for('home'))
-  return render_template(
-    'register.html',
-    form=form,
-    title='Register'
-  )
+
+  return render_template('register.html', form=form, title='Register')
+
+# Login route
+# -----------------------
 
 @app.route("/login", methods=['GET','POST'])
 def login():
   form = LoginForm()
+
   if form.validate_on_submit():
     if form.email.data == 'admin@blog.com' and form.password.data == 'password':
         flash('You have been logged in!', 'success')
         return redirect(url_for('home'))
     else:
         flash('Login Unsuccessful. Please check username and password', 'danger')
-  return render_template(
-    'login.html',
-    form=form,
-    title='Login'
-  )
+
+  return render_template('login.html', form=form, title='Login')
